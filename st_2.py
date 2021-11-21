72b35c844035bc3cb73e2072683ac9c4cdd5ee5c
cveatt <usernasty09@gmail.com>
2021-11-21 19:54:09 +0200

    use

--- st_2.py
+++ st_2.py
@@ -9,24 +9,23 @@ def load_model():
     return SentimentModel()
 
 
-    if __name__ == "__main__":
-        sentiment_model = load_model()
-        sentiment_map = {
-            1: "Positive",
-            0: "Neutral",
-            -1: "Negative"
-        }
-
-        st.title("SentimentModel")
+if __name__ == "__main__":
+    sentiment_model = load_model()
+    sentiment_map = {
+         1: "Positive",
+         0: "Neutral",
+         -1: "Negative"
+    }
+    st.title("SentimentModel")
         
-        with st.form("my_form"):
-            text = st.text_area("text")
+    with st.form("my_form"):
+        text = st.text_area("text")
 
-            submitted = st.form_submit_button("Submit")
-            if submitted:
-                sentiment = sentiment_model(text=text)
-                st.write(f"Sentiment: {sentiment_map[sentiment]}")
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            sentiment = sentiment_model(text=text)
+            st.write(f"Sentiment: {sentiment_map[sentiment]}")
 
-                if sentiment == 1:
-                    st.balloons()
+            if sentiment == 1:
+                st.balloons()
                     
